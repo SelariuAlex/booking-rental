92098f215b105d798f536295a503fd3a726c4005
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-05-30 16:45:46 +0300

    setting up routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
 import Header from './shared/Header';
 import RentalList from './components/rental/RentalList';
+import RentalDetail from './components/rental/RentalDetail';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <div className="container">
-        <RentalList />
-      </div>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Header />
+          <div className="container">
+            <Route exact path="/" component={RentalList} />
+            <Route exact path="/test" component={RentalDetail} />
+          </div>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
