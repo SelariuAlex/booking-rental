5a5ea6baf8e651c23512132408ebf0a6ccb8ebdd
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-05-31 17:14:26 +0300

    rental data

--- src/components/rental/RentalCard.js
+++ src/components/rental/RentalCard.js
@@ -1,18 +1,21 @@
 import React from 'react';
 
-function RentalCard() {
+function RentalCard(props) {
+  const rental = props.rental;
+
   return (
     <div className="col-md-3 col-xs-6">
       <div className="card bwm-card">
-        <img
-          className="card-img-top"
-          src="http://via.placeholder.com/350x250"
-          alt=""
-        />
+        <img className="card-img-top" src={rental.image} alt="" />
         <div className="card-block">
-          <h6 className="card-subtitle">Whole Apartment &#183; Timisoara</h6>
-          <h4 className="card-title">Some nice apartment</h4>
-          <p className="card-text">$40 per Night &#183; Free Cancelation</p>
+          <h6 className="card-subtitle">
+            {rental.shared ? 'shared' : 'whole'} {rental.category} &#183;{' '}
+            {rental.city}
+          </h6>
+          <h4 className="card-title">{rental.title}</h4>
+          <p className="card-text">
+            ${rental.dailyRate} per Night &#183; Free Cancelation
+          </p>
           <a href="" className="card-link">
             More Info
           </a>
--- src/components/rental/RentalList.js
+++ src/components/rental/RentalList.js
@@ -3,12 +3,65 @@ import RentalCard from './RentalCard';
 
 class RentalList extends Component {
   state = {
-    rentals: [1, 2, 3]
+    rentals: [
+      {
+        id: 1,
+        title: 'Central Apartment',
+        city: 'Timisoara',
+        street: 'Muzicescu',
+        category: 'apartment',
+        image: 'http://via.placeholder.com/350x250',
+        bedrooms: 3,
+        description: 'Very nice apartment',
+        dailyRate: 34,
+        shared: false,
+        createdAt: '24/12/2017'
+      },
+      {
+        id: 2,
+        title: 'Central Apartment 2',
+        city: 'Remetea',
+        street: 'Main street',
+        category: 'condo',
+        image: 'http://via.placeholder.com/350x250',
+        bedrooms: 2,
+        description: 'Very nice apartment',
+        dailyRate: 12,
+        shared: true,
+        createdAt: '24/12/2017'
+      },
+      {
+        id: 3,
+        title: 'Central Apartment 3',
+        city: 'Arad',
+        street: 'Timisoarei',
+        category: 'condo',
+        image: 'http://via.placeholder.com/350x250',
+        bedrooms: 2,
+        description: 'Very nice apartment',
+        dailyRate: 34,
+        shared: true,
+        createdAt: '24/12/2017'
+      },
+      {
+        id: 4,
+        title: 'Central Apartment 4',
+        city: 'Timisoara',
+        street: '1 Decembrie',
+        category: 'house',
+        image: 'http://via.placeholder.com/350x250',
+        bedrooms: 9,
+        description: 'Very nice apartment',
+        dailyRate: 33,
+        shared: true,
+        createdAt: '24/12/2017'
+      }
+    ]
   };
 
   renderRentals() {
     return this.state.rentals.map(rental => {
-      return <RentalCard key={Math.random()} />;
+      return <RentalCard key={Math.random()} rental={rental} />;
     });
   }
 
