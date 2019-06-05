b4bde3275550c818e53e443ce7dda6d0bb7239f9
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-06-05 12:14:07 +0300

    setting up express, postman, atlas moongoDB

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/rentals', (req, res) => {
+  res.json({ success: true });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function() {
+  console.log('Running');
+});
