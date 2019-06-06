3453cf46e5b233b89baec6afb4df40ebced8d465
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-06-06 12:46:54 +0300

    push data to DB

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
 const config = require('./config/dev');
+const FakeDb = require('./models/fake-db');
 
-mongoose.connect(config.DB_URI, { useNewUrlParser: true });
+mongoose.connect(config.DB_URI, { useNewUrlParser: true }).then(() => {
+  const fakeDb = new FakeDb();
+  fakeDb.seedDb();
+});
 
 const app = express();
 
---
+++ server/models/fake-db.js
@@ -0,0 +1,57 @@
+const Rental = require('./rental');
+
+class FakeDb {
+  constructor() {
+    this.rentals = [
+      {
+        title: 'Nice view of park',
+        city: 'Timisoara',
+        street: 'Muzicescu',
+        category: 'condo',
+        image:
+          'https://booksync-jerga-prod.s3.amazonaws.com/uploads/rental/image/5/image.jpeg',
+        bedrooms: 4,
+        shared: true,
+        description: 'Very nice condo.',
+        dailyRate: 15
+      },
+      {
+        title: 'Modern apartment in center',
+        city: 'Timisoara',
+        street: 'Traian',
+        category: 'apartment',
+        image:
+          'https://booksync-jerga-prod.s3.amazonaws.com/uploads/rental/image/5/image.jpeg',
+        bedrooms: 1,
+        shared: false,
+        description: 'Nice apartment in center of the city.',
+        dailyRate: 11
+      },
+      {
+        title: 'Old house in nature',
+        city: 'Remetea Mare',
+        street: 'Main street nr 32',
+        category: 'house',
+        image:
+          'https://booksync-jerga-prod.s3.amazonaws.com/uploads/rental/image/5/image.jpeg',
+        bedrooms: 5,
+        shared: true,
+        description: 'House in nature.',
+        dailyRate: 23
+      }
+    ];
+  }
+
+  pushRentalsTodb() {
+    this.rentals.forEach(rental => {
+      const newRental = new Rental(rental);
+      newRental.save();
+    });
+  }
+
+  seedDb() {
+    this.pushRentalsTodb();
+  }
+}
+
+module.exports = FakeDb;
