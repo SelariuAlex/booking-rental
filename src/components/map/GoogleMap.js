002cd4babf0c58cf286b6cdb891d421d2a3f2783
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-06-10 10:32:20 +0300

    geolocate coordinates

--- src/components/map/GoogleMap.js
+++ src/components/map/GoogleMap.js
@@ -8,17 +8,41 @@ import {
 } from 'react-google-maps';
 
 const MapComponent = props => {
+  const coordinates = props.coordinates;
   return (
-    <GoogleMap defaultZoom={8} defaultCenter={{ lat: 45.749, lng: 21.227 }}>
-      <Marker position={{ lat: 45.749, lng: 21.227 }} />
+    <GoogleMap defaultZoom={8} defaultCenter={coordinates} center={coordinates}>
+      <Marker position={coordinates} />
     </GoogleMap>
   );
 };
 
 function withGeocode(WrappedComponent) {
   return class extends Component {
+    state = {
+      coordinates: {
+        lat: 0,
+        lng: 0
+      }
+    };
+
+    componentWillMount() {
+      this.geocodeLocation();
+    }
+
+    geocodeLocation = () => {
+      const location = this.props.location;
+      const geocoder = new window.google.maps.Geocoder();
+      geocoder.geocode({ address: location }, (result, status) => {
+        if (status === 'OK') {
+          const geometry = result[0].geometry.location;
+          const coordinates = { lat: geometry.lat(), lng: geometry.lng() };
+          this.setState({ coordinates: coordinates });
+        }
+      });
+    };
+
     render() {
-      return <WrappedComponent />;
+      return <WrappedComponent {...this.state} />;
     }
   };
 }
