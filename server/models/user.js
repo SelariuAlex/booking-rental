82717d15ece897c792893b890252bebbbf561bb6
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-06-13 13:37:02 +0300

    hashing password with bcrypt

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
   usermane: {
@@ -23,6 +24,17 @@ const userSchema = new Schema({
     required: 'Password is required'
   },
   rentals: [{ type: Schema.Types.ObjectId, ref: 'Rental' }]
+});
+
+userSchema.pre('save', function(next) {
+  const user = this;
+
+  bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      user.password = hash;
+      next();
+    });
+  });
 });
 
 module.exports = mongoose.model('User', userSchema);
