0d0e77aaf0a0f705d896c0fe4c3afe10a0f35719
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-06-20 14:40:16 +0300

    authentication with JWT

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,7 +1,48 @@
 const User = require('../models/user');
 const MongooseHelpers = require('../helpers/mongoose');
+const jwt = require('jsonwebtoken');
+const config = require('../config/dev');
 
-exports.auth = function(req, res) {};
+exports.auth = function(req, res) {
+  const { email, password } = req.body;
+
+  if (!password || !email) {
+    return res.status(422).send({
+      errors: [{ title: 'Data missing', detail: 'Provide email and password' }]
+    });
+  }
+
+  User.findOne({ email }, function(err, user) {
+    if (err) {
+      return res.status(422).send({
+        errors: MongooseHelpers.normalizeErrors(err.errors)
+      });
+    }
+
+    if (!user) {
+      return res.status(422).send({
+        errors: [{ title: 'Invalid user', detail: 'User does not exist' }]
+      });
+    }
+
+    if (user.hasSamePassword(password)) {
+      const token = jwt.sign(
+        {
+          userId: user.id,
+          username: user.username
+        },
+        config.SECRET,
+        { expiresIn: '100h' }
+      );
+
+      return res.json(token);
+    } else {
+      return res.status(422).send({
+        errors: [{ title: 'Wrong Data', detail: 'Wrong email or password' }]
+      });
+    }
+  });
+};
 
 exports.register = function(req, res) {
   const { username, email, password, passwordConfirmation } = req.body;
--- server/models/user.js
+++ server/models/user.js
@@ -26,6 +26,10 @@ const userSchema = new Schema({
   rentals: [{ type: Schema.Types.ObjectId, ref: 'Rental' }]
 });
 
+userSchema.methods.hasSamePassword = function(requiredPassword) {
+  return bcrypt.compareSync(requiredPassword, this.password);
+};
+
 userSchema.pre('save', function(next) {
   const user = this;
 
