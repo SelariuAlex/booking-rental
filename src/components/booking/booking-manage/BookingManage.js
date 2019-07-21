3e8433834ed9d9e8e7c5e0efb14030ff3e2193a3
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-07-21 12:01:37 +0300

    manage booking card

--- src/components/booking/booking-manage/BookingManage.js
+++ src/components/booking/booking-manage/BookingManage.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { prettifyDate } from '../../../helpers';
+import { BookingCard } from './BookingCard';
 
 import * as actions from '../../../actions';
 
@@ -10,61 +10,19 @@ class BookingManage extends Component {
     this.props.dispatch(actions.fetchUserBookings());
   }
 
+  renderBookings(bookings) {
+    return bookings.map((booking, index) => (
+      <BookingCard booking={booking} key={index} />
+    ));
+  }
+
   render() {
     const { data: bookings, isFetching } = this.props.userBookings;
 
     return (
       <section id="userBookings">
         <h1 className="page-title">My Bookings</h1>
-        <div className="row">
-          {bookings.map((booking, index) => {
-            return (
-              <div key={index} className="col-md-4">
-                <div className="card text-center">
-                  <div className="card-header">
-                    {booking.rental
-                      ? booking.rental.category
-                      : 'Deleted Rental'}
-                  </div>
-                  <div className="card-block">
-                    {booking.rental && (
-                      <div>
-                        <h4 className="card-title">
-                          {' '}
-                          {booking.rental.title} - {booking.rental.city}
-                        </h4>
-                        <p className="card-text booking-desc">
-                          {booking.rental.description}
-                        </p>
-                      </div>
-                    )}
-                    <p className="card-text booking-days">
-                      {prettifyDate(booking.startAt)} -{' '}
-                      {prettifyDate(booking.endAt)} | {booking.days} days
-                    </p>
-                    <p className="card-text booking-price">
-                      <span>Price: </span>{' '}
-                      <span className="booking-price-value">
-                        {booking.totalPrice} $
-                      </span>
-                    </p>
-                    {booking.rental && (
-                      <Link
-                        className="btn btn-bwm"
-                        to={`/rentals/${booking.rental._id}`}
-                      >
-                        Go to Rental
-                      </Link>
-                    )}
-                  </div>
-                  <div className="card-footer text-muted">
-                    Created {prettifyDate(booking.createdAt)}
-                  </div>
-                </div>
-              </div>
-            );
-          })}
-        </div>
+        <div className="row">{this.renderBookings(bookings)}</div>
         {!isFetching && bookings.length === 0 && (
           <div className="alert alert-warning">
             You have no bookings created go to rentals section and book your
