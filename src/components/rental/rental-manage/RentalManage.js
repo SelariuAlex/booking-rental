650069543ff6d0eb58f749fc729b71b82c011763
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-07-21 12:41:04 +0300

    manage rental card

--- src/components/rental/rental-manage/RentalManage.js
+++ src/components/rental/rental-manage/RentalManage.js
@@ -1,29 +1,51 @@
 import React, { Component } from 'react';
 import * as actions from '../../../actions';
+import { Link } from 'react-router-dom';
+import RentalManageCard from './RentalManageCard';
 
 class RentalManage extends Component {
   state = {
     userRentals: [],
-    errors: []
+    errors: [],
+    isFetching: false
   };
 
   componentWillMount() {
+    this.setState({ isFetching: true });
     actions
       .getUserRentals()
       .then(
-        userRentals => this.setState({ userRentals }),
-        errors => this.setState({ errors })
+        userRentals => this.setState({ userRentals, isFetching: false }),
+        errors => this.setState({ errors, isFetching: false })
       );
   }
 
+  renderRentalCards(rentals) {
+    return rentals.map((rental, index) => (
+      <RentalManageCard key={index} rental={rental} rentalIndex={index} />
+    ));
+  }
+
   render() {
-    const { userRentals } = this.state;
+    const { userRentals, isFetching } = this.state;
     return (
-      <div>
-        {userRentals.map((rental, index) => (
-          <p key={index}>{rental.title}</p>
-        ))}
-      </div>
+      <section id="userRentals">
+        <h1 className="page-title">My Rentals</h1>
+        <div className="row">{this.renderRentalCards(userRentals)}</div>
+        {!isFetching && userRentals.length === 0 && (
+          <div className="alert alert-warning">
+            You dont have any rentals currenty created. If you want advertised
+            your property please follow this link.
+            <Link
+              style={{ marginLeft: '10px' }}
+              className="btn btn-bwm"
+              to="/rentals/new"
+            >
+              Register Rental
+            </Link>
+          </div>
+        )}
+      </section>
     );
   }
 }
