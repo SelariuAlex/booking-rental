535f6a26cb6eb61dcb16e8c4088bef3eea597dbc
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-12-09 15:58:15 +0200

    featured card

---
+++ src/components/featured/featuredCard/FeaturedCard.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const FeaturedCard = () => {
+  return (
+    <div class="card-body">
+      <div class="card-title text-capitalize">
+        <h5 class="mb-3">excellent property in Timisoara</h5>
+        <p>
+          <span class="text-blue">
+            <i class="fas fa-map-marker-alt"></i>
+          </span>
+          adress
+        </p>
+      </div>
+    </div>
+  );
+};
